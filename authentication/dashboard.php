079db31c8a3c9091ccf9e68fc9a71b88d3cfed9e
Venusri76 <bvenu1316@gmail.com>
2026-02-11 12:12:31 -0600

    Authentication for user login page

---
+++ authentication/dashboard.php
@@ -0,0 +1,39 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['user_id']))
+    {
+     header("Location: login.php");
+     exit();
+    }
+  
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if($_POST['log_out_btn']){
+      
+      session_unset();
+      session_destroy();
+      header("Location: login.php");
+      exit();
+    }
+  }
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>CSCI 6040</title>
+  <link rel="stylesheet" href="custom_style.css">
+</head>
+<body>
+  <div id="content_div">
+    <h1>Welcome to CSCI 6040</h1>
+    <h2>Dashboard Under-contstruction</h2>
+    <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <h3>User Information</h3>
+   <h4>User name:<?php echo htmlspecialchars($_SESSION['user_name']); ?></h4>
+  <h4>Email: <?php echo htmlspecialchars($_SESSION['user_email']); ?></h4>
+      <input type="submit" id="submit_btn" name="log_out_btn" value="Log Out">
+    </form>
+  </div>
+</body>
+</html>
